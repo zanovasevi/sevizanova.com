9cc74ea317a3e82bcd0000a0c1fd989fa41db9b6
zanovasevi <zanovasevi@gmail.com>
2022-02-22 11:38:58 +0200

    Changed picture and phone

---
+++ obj/Release/net5.0/PubTmp/Out/wwwroot/js/coursesJS.js
@@ -0,0 +1,29 @@
+﻿let buttons = document.getElementsByTagName('button');
+let texts = document.getElementsByClassName('text');
+
+
+for (let but of buttons) {
+    but.addEventListener('click', onClick);
+}
+
+for (let text of texts) {
+    text.style.display = 'none';
+}
+
+function onClick(ev) {
+    ev.preventDefault();
+
+    let hiddenText = ev.target.parentElement.parentElement.getElementsByTagName('span')[4];
+    let points = ev.target.parentElement.getElementsByTagName('span')[0];
+
+    if (ev.target.textContent == 'show more') {
+        ev.target.textContent = 'show less';
+        hiddenText.style.display = '';
+        points.style.visibility = 'hidden';
+
+    } else {
+        ev.target.textContent = 'show more';
+        hiddenText.style.display = 'none';
+        points.style.visibility = '';
+    }
+}
